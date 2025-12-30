6f4f255ba1d245e639c89c03e1bd434d93a1ba20
CarthDwigth <adqf02@gmail.com>
2025-12-30 02:10:02 -0400

    Actualización automática: 30/12/2025 02:10

--- db.py
+++ db.py
@@ -4,6 +4,7 @@ import os
 def get_db():
     # Render te da la URL completa en esta variable
     db_url = os.environ.get("DATABASE_URL")
+    crear_tablas()
     
     # Esto permite que se conecte tanto en local como en la nube
     conn = psycopg2.connect(db_url)
